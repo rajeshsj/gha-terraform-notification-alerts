172afa179ec78b559892efa1376f13d79747ae7a
Rajesh <32957821+rajeshsj@users.noreply.github.com>
2023-08-11 21:33:18 +0530

    Update main.tf

--- notification-setup/main.tf
+++ notification-setup/main.tf
@@ -1,6 +1,6 @@
 module "notification_channels" {
   source                        = "../"
-  project                       = "gcp-abs-svav-dev-prj-01"
+  project                       = "test-project-dec19"
   notification_channels         = yamldecode(file("channels.yaml"))
   #slack_token_secret_project_id = "my-project-123" # Optional; to provide token with gcp stored secret
   #slack_token_secret_name       = "my-slack-token" # Optional; to provide token with gcp stored secret
