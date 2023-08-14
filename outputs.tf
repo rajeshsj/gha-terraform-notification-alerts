bc3afe0f9dd0b03b47d9f969bfc1618d99f9584c
Rajesh <32957821+rajeshsj@users.noreply.github.com>
2023-08-15 00:22:38 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@
 
 output "notification_channels" {
   value = merge(
-    { for nc in google_monitoring_notification_channel.sms : nc.display_name => nc.id },
-    { for nc in google_monitoring_notification_channel.webhook_tokenauth : nc.display_name => nc.id },
-    { for nc in google_monitoring_notification_channel.webhook_basicauth : nc.display_name => nc.id },
+    #{ for nc in google_monitoring_notification_channel.sms : nc.display_name => nc.id },
+    #{ for nc in google_monitoring_notification_channel.webhook_tokenauth : nc.display_name => nc.id },
+    #{ for nc in google_monitoring_notification_channel.webhook_basicauth : nc.display_name => nc.id },
     { for nc in google_monitoring_notification_channel.email : nc.display_name => nc.id },
   )
 
@@ -13,9 +13,9 @@ output "notification_channels" {
 
 output "fallback_channels_ids" {
   value = concat(
-    [for nc in google_monitoring_notification_channel.sms : nc.id if try(nc.user_labels.fallback_channel, false) ],
-    [for nc in google_monitoring_notification_channel.webhook_tokenauth : nc.id if try(nc.user_labels.fallback_channel, false) ],
-    [for nc in google_monitoring_notification_channel.webhook_basicauth : nc.id if try(nc.user_labels.fallback_channel, false) ],
+    #[for nc in google_monitoring_notification_channel.sms : nc.id if try(nc.user_labels.fallback_channel, false) ],
+    #[for nc in google_monitoring_notification_channel.webhook_tokenauth : nc.id if try(nc.user_labels.fallback_channel, false) ],
+    #[for nc in google_monitoring_notification_channel.webhook_basicauth : nc.id if try(nc.user_labels.fallback_channel, false) ],
     [for nc in google_monitoring_notification_channel.email : nc.id if try(nc.user_labels.fallback_channel, false) ],
   )
 
