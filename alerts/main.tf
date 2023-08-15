3e298e52b12571f810a312d24e09847f27db2c8d
Rajesh <32957821+rajeshsj@users.noreply.github.com>
2023-08-15 13:43:17 +0530

    Update main.tf

--- alerts/main.tf
+++ alerts/main.tf
@@ -2,11 +2,11 @@ module "alert" {
   source                         = "../"
   project                        = "test-project-dec19"
   policies                       = yamldecode(file("alerts.yaml"))
-  fallback_notification_channels = ["fallback-channel", "projects/test-project-dec19/notificationChannels/2897121121673085037]"]
- # notification_channel_ids = {
+ # fallback_notification_channels = ["fallback-channel", "projects/test-project-dec19/notificationChannels/2897121121673085037]"]
+  notification_channel_ids = {
     #"fallback-channel" : "projects/test-project-dec19/notificationChannels/2897121121673085037"
-    #notification_channel : "projects/test-project-dec19/notificationChannels/2897121121673085037"
- # }
+    notification_channel : "projects/test-project-dec19/notificationChannels/2897121121673085037"
+  }
 
   default_user_labels = {
     cc = "all-alerts-will-have-this-label"
